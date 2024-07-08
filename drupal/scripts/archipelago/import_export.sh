9c4396b19bdc7be10c5680fe7845ead3c06813ae
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-07-08 14:50:13 -0400

    Update import/export script to deal with versions and UUIDs too

--- drupal/scripts/archipelago/import_export.sh
+++ drupal/scripts/archipelago/import_export.sh
@@ -30,6 +30,18 @@ Help()
    echo
 }
 
+if ! command -v jq &> /dev/null
+then
+    echo "jq command could not be found. Please install or configure jq."
+    exit
+fi
+
+if ! command -v json_pp &> /dev/null
+then
+    echo "json_pp command could not be found. Please install or configure json_pp."
+    exit
+fi
+
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 adl_env_file="$script_dir/../../../deploy/ec2-docker/.env"
 ad_env_file="$script_dir/../../.env"
@@ -117,10 +129,10 @@ export_data() {
   fi
   cd "$source_target" &&
   echo "Exporting Metadata Displays to $source_target"
-  curl -w "\n" -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -K - "$json_api_endpoint" <<< "user = \"$username:$password\"" | jq -cr '.data[]|del(.links)|del(.relationships)|del(.attributes.created)|del(.attributes.drupal_internal__id)|del(.attributes.changed)|del(.attributes.link)|{"data": .}' | while read -r metadatadisplay_entity;
+  curl -w "\n" -H 'Accept: application/vnd.api+json' -H 'Content-type: application/vnd.api+json' -K - "$json_api_endpoint" <<< "user = \"$username:$password\"" | jq -cr '.data[]|del(.links)|del(.relationships)|del(.attributes.created)|del(.attributes.drupal_internal__id)|del(.attributes.drupal_internal__vid)|del(.attributes.revision_created)|del(.attributes.revision_log_message)|del(.attributes.revision_translation_affected)|del(.attributes.changed)|del(.attributes.link)|{"data": .}' | while read -r metadatadisplay_entity;
   do
     uuid=$(echo "$metadatadisplay_entity" | jq -r .data.id)
-    echo "$metadatadisplay_entity" >> "metadatadisplay_entity_$uuid.json"
+    echo "$metadatadisplay_entity" | json_pp -json_opt escape_slash >> "metadatadisplay_entity_$uuid.json"
   done
 }
 
